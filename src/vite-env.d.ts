db5e1903a42755621e2bbecdce62331791f6f20d
entertainmentinnanetclout-cpu <entertainmentinnanetclout@gmail.com>
2025-11-09 15:16:54 +0200

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
+# Reference for Vite
 /// <reference types="vite/client" />
+
+# Supabase project URL
+VITE_SUPABASE_URL=https://mefjzkhobkltlbmhusdh.supabase.co
+
+# Supabase anon/public API key
+VITE_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1lZmp6a2hvYmtsdGxibWh1c2RoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAzMTE5ODYsImV4cCI6MjA3NTg4Nzk4Nn0.h9VlKqtA4QMidLh_FbIiNviZRzeLe4OsBs1omh3Jy6U
