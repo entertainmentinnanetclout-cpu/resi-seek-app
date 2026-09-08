ed7270ae8d4cc3e39f0150d3fba7be5a26915b81
entertainmentinnanetclout-cpu <entertainmentinnanetclout@gmail.com>
2026-09-08 05:27:58 +0200

    fix(resmap): route Find My Res to resilient map engine

--- src/components/findmyres/SmartSearchBar.tsx
+++ src/components/findmyres/SmartSearchBar.tsx
@@ -8,7 +8,7 @@ import { Switch } from "@/components/ui/switch";
 import { Label } from "@/components/ui/label";
 import { getCampusOptions } from "@/constants/institutionOptions";
 import type { ResidenceFilters } from "@/hooks/useResidenceFilters";
-import ResMapExperience from "@/components/resmap/ResMapExperience";
+import ResMapExperience from "@/components/resmap/ResMapExperienceReliable";
 import ResDiscoveryEngine from "@/components/resmap/ResDiscoveryEngine";
 
 interface SmartSearchBarProps {
