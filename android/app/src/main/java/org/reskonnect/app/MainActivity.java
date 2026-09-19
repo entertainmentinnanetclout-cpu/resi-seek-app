dc2aedc7a032e45f606446eb7a29b46f96be1469
entertainmentinnanetclout-cpu <entertainmentinnanetclout@gmail.com>
2026-09-19 07:42:12 +0200

    fix(android): recover WebView renderer instead of terminating app

--- android/app/src/main/java/org/reskonnect/app/MainActivity.java
+++ android/app/src/main/java/org/reskonnect/app/MainActivity.java
@@ -1,5 +1,40 @@
 package org.reskonnect.app;
 
-import com.getcapacitor.BridgeActivity;
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.RenderProcessGoneDetail;
+import android.webkit.WebView;
 
-public class MainActivity extends BridgeActivity {}
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.BridgeWebViewClient;
+
+public class MainActivity extends BridgeActivity {
+    private boolean rendererRecoveryScheduled = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final WebView webView = bridge.getWebView();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            webView.setRendererPriorityPolicy(WebView.RENDERER_PRIORITY_IMPORTANT, true);
+        }
+
+        webView.setWebViewClient(new BridgeWebViewClient(bridge) {
+            @Override
+            public boolean onRenderProcessGone(WebView view, RenderProcessGoneDetail detail) {
+                getSharedPreferences("reskonnect_runtime", MODE_PRIVATE)
+                    .edit()
+                    .putLong("last_renderer_loss_at", System.currentTimeMillis())
+                    .putBoolean("last_renderer_did_crash", detail != null && detail.didCrash())
+                    .apply();
+
+                if (!rendererRecoveryScheduled && !isFinishing()) {
+                    rendererRecoveryScheduled = true;
+                    runOnUiThread(() -> recreate());
+                }
+                return true;
+            }
+        });
+    }
+}
